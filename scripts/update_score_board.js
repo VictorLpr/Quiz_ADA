fd8b3a880d984aa502e8bf47cc41ddf17b8a4f1d
laanatri <laanatri.ade@gmail.com>
2025-02-24 15:31:07 +0100

    ajout images logo score board

--- scripts/update_score_board.js
+++ scripts/update_score_board.js
@@ -1,7 +1,7 @@
 
 const scoreBoard = document.querySelector(".score-board table tbody");
 let gameNumber = localStorage.getItem("gameNumber");
-let players =[]
+let players =[];
 
 for (let i = 1; i <= gameNumber; i++) {
     players.push(JSON.parse(localStorage.getItem(i)));
@@ -10,12 +10,17 @@ for (let i = 1; i <= gameNumber; i++) {
 
 players.sort((b, a) => a.score - b.score);
 
-players.forEach((player) => {
-    scoreBoard.insertAdjacentHTML("beforeend", `<tr>
+players.forEach((player, i) => {
+    scoreBoard.insertAdjacentHTML("beforeend", `<tr data-index="${i}">
     <th class="player-name">${player.name}</th>
     <td class="player-score">${player.score}</td>
-    
-    <td class="thèmes"></td>
+    <td class="themes"></td>
 </tr>`
     )
-})
+
+    const scoreBoardThemes = document.querySelector(`.score-board table tbody [data-index="${i}"] .themes`);
+
+    player.theme.forEach(themeURL => {
+        scoreBoardThemes.insertAdjacentHTML("beforeend", `<img src="${themeURL}" alt="">`);
+    })
+});
